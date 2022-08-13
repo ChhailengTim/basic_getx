79b48ac505a0f241d7a71757a89b028957830394
idev_group <chhailengtim2018@gmail.com>
2022-08-13 12:04:44 +0700

    fetch

--- api_fetch/lib/main.dart
+++ api_fetch/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:api_fetch/good/view/good_screend.dart';
+import 'package:api_fetch/brand/view/brand_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GoodScreen(),
+      home: BrandScreen(),
     );
   }
 }
