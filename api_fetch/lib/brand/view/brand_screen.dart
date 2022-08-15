58d9916d2fd6a153c9e5fbe29e928d4b42447a5c
idev_group <chhailengtim2018@gmail.com>
2022-08-15 11:14:03 +0700

    Update brand_screen.dart

--- api_fetch/lib/brand/view/brand_screen.dart
+++ api_fetch/lib/brand/view/brand_screen.dart
@@ -1,4 +1,4 @@
-import 'package:api_fetch/brand/controller/brand_controller.dart';
+import 'package:api_fetch/brand/controller/post_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,14 +7,20 @@ class BrandScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final BrandController brandController = Get.put(BrandController());
+    final PostController brandController = Get.put(PostController());
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            brandController.getBrandData();
-          },
-          child:  const Text('GetData'),
+      body: SafeArea(
+        child: Expanded(
+          child: ListView.builder(
+            itemCount: brandController.postList.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  title: Text('${brandController.data[index].title}'),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
