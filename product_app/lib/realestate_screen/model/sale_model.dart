e0b633a771ba2b884ae01822b1e8438e2617d533
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-18 15:38:22 +0700

    Create sale_model.dart

---
+++ product_app/lib/realestate_screen/model/sale_model.dart
@@ -0,0 +1,16 @@
+class SaleModel {
+  final String? title;
+  final String? price;
+  final String? data;
+  final String? location;
+  final String? highway;
+  final String? status;
+  SaleModel({
+    this.title,
+    this.price,
+    this.data,
+    this.location,
+    this.highway,
+    this.status,
+  });
+}
