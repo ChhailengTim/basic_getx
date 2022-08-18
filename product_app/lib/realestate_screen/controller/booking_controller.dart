66bb391396ac60cb246807aaf855e95d03c21fbd
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-18 15:38:16 +0700

    Create booking_controller.dart

---
+++ product_app/lib/realestate_screen/controller/booking_controller.dart
@@ -0,0 +1,97 @@
+import 'package:get/get.dart';
+import 'package:product_app/realestate_screen/model/booking_model.dart';
+
+class BookingController extends GetxController {
+  final bookingList = <BookingModel>[
+    BookingModel(
+      title: 'A01',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: '',
+    ),
+    BookingModel(
+      title: 'A02',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: 'Booked',
+    ),
+    BookingModel(
+      title: 'A03',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: '',
+    ),
+    BookingModel(
+      title: 'A04',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: '',
+    ),
+    BookingModel(
+      title: 'A05',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: '',
+    ),
+    BookingModel(
+      title: 'A06',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: '',
+    ),
+    BookingModel(
+      title: 'A07',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: 'Sold',
+    ),
+    BookingModel(
+      title: 'A08',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: 'Booked',
+    ),
+    BookingModel(
+      title: 'A09',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: 'Sold',
+    ),
+    BookingModel(
+      title: 'A010',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: 'Booked',
+    ),
+    BookingModel(
+      title: 'A011',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: '',
+    ),
+    BookingModel(
+      title: 'A12',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: '',
+    ),
+    BookingModel(
+      title: 'A13',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: 'Booked',
+    ),
+    BookingModel(
+      title: 'A14',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: '',
+    ),
+    BookingModel(
+      title: 'A15',
+      sqr: '278 m2',
+      price: '19,200,22 \$',
+      status: 'Booked',
+    ),
+  ];
+}
