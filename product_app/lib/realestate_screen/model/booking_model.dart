ce498a952fbcf0d9c1ef6c5b498708461f9a27a8
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-18 15:38:20 +0700

    Create booking_model.dart

---
+++ product_app/lib/realestate_screen/model/booking_model.dart
@@ -0,0 +1,12 @@
+class BookingModel {
+  final String? title;
+  final String? sqr;
+  final String? price;
+  final String? status;
+  BookingModel({
+    this.title,
+    this.sqr,
+    this.price,
+    this.status,
+  });
+}
