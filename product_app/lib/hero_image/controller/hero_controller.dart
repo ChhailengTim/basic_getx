88aca1525ad606765960f9176855482117d19d79
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-19 14:40:04 +0700

    hero images animation

---
+++ product_app/lib/hero_image/controller/hero_controller.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import 'package:product_app/hero_image/model/hero_model.dart';
+
+class HeroControllers extends GetxController {
+  final listHero = <HeroModel>[
+    HeroModel(
+      title: 'Cat1',
+      image: 'assets/images/cat1.jpg',
+    ),
+    HeroModel(
+      title: 'Cat2',
+      image: 'assets/images/cat2.png',
+    ),
+    HeroModel(
+      title: 'Cat3',
+      image: 'assets/images/cat3.jpg',
+    ),
+    HeroModel(
+      title: 'Cat4',
+      image: 'assets/images/cat4.jpg',
+    ),
+    HeroModel(
+      title: 'Cat5',
+      image: 'assets/images/cat5.jpg',
+    ),
+  ];
+}
---
+++ product_app/lib/hero_image/model/hero_model.dart
@@ -0,0 +1,8 @@
+class HeroModel {
+  final String? title;
+  final String? image;
+  HeroModel({
+    this.title,
+    this.image,
+  });
+}
