cee847223f0daf1d9feb128625c163a5ff10949c
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-25 14:22:23 +0700

    Create swiper_controller.dart

---
+++ product_app/lib/swiper/controller/swiper_controller.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+import 'package:product_app/swiper/model/swiper_model.dart';
+
+class SwiperControllers extends GetxController {
+  final listSwiper = <SwiperModel>[
+    SwiperModel(
+      title: 'Cat1',
+      image: 'assets/images/cat1.jpg',
+    ),
+    SwiperModel(
+      title: 'Cat2',
+      image: 'assets/images/cat2.png',
+    ),
+    SwiperModel(
+      title: 'Cat3',
+      image: 'assets/images/cat3.jpg',
+    ),
+    SwiperModel(
+      title: 'Cat4',
+      image: 'assets/images/cat4.jpg',
+    ),
+    SwiperModel(
+      title: 'Cat5',
+      image: 'assets/images/cat5.jpg',
+    ),
+    SwiperModel(
+      title: 'Cat6',
+      image: 'assets/images/cat6.jpg',
+    ),
+  ];
+}
