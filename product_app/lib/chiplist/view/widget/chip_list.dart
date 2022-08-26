c83d215ab0c8398baadf19067fd04dffbcb347c5
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-26 11:59:37 +0700

    Create chip_list.dart

---
+++ product_app/lib/chiplist/view/widget/chip_list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ChipList extends StatelessWidget {
+  ChipList({
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
+  final VoidCallback onPressed;
+
+  final List<String> chipList = [
+    'All',
+    'Pending',
+    'Approved',
+    'Rejected',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      physics: const BouncingScrollPhysics(),
+      child: Row(
+        children: chipList
+            .asMap()
+            .entries
+            .map(
+              (e) => Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: ActionChip(
+                  label: Text(
+                    e.value,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
+                  ),
+                  onPressed: onPressed,
+                ),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
