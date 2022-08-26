627d636da7cf5410336259cdd13abf59b44d620f
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-26 11:59:29 +0700

    Create chip_model.dart

---
+++ product_app/lib/chiplist/model/chip_model.dart
@@ -0,0 +1,16 @@
+class OrderModel {
+  final String? title;
+  final String? price;
+  final String? date;
+  final String? status;
+  final String? code;
+  final String? locate;
+  OrderModel({
+    this.title,
+    this.price,
+    this.date,
+    this.status,
+    this.code,
+    this.locate,
+  });
+}
