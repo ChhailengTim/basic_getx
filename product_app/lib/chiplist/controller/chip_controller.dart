f400411de240dfb3d4436083ea3bb90796385a35
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-26 11:59:28 +0700

    Create chip_controller.dart

---
+++ product_app/lib/chiplist/controller/chip_controller.dart
@@ -0,0 +1,78 @@
+import 'package:get/get.dart';
+import 'package:product_app/chiplist/model/chip_model.dart';
+
+
+class OrderController extends GetxController {
+  final listOrder = <OrderModel>[
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Pending',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Rejected',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Approved',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Pending',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Rejected',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Pending',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Approved',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Pending',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Pending',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+    OrderModel(
+        title: 'A01',
+        price: '\$50.000',
+        date: '22 Dec, 20:40',
+        status: 'Rejected',
+        code: 'm54f_vwp',
+        locate: ' Phnom penh, Cambodia'),
+  ];
+}
