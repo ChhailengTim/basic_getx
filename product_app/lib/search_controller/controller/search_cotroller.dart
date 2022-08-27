388c85d42cab8649ea7eaaf16a68803b2869dcd5
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-27 11:46:00 +0700

    Create search_cotroller.dart

---
+++ product_app/lib/search_controller/controller/search_cotroller.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:product_app/search_controller/model/search_cmodel.dart';
+
+class SearchController extends GetxController {
+  final controller = TextEditingController();
+  List<CatModel> allCatModel = [
+    CatModel(
+      title: 'Meow Meow',
+      imageURL: 'assets/images/cat1.jpg',
+    ),
+    CatModel(
+      title: 'Cat Meow',
+      imageURL: 'assets/images/cat2.png',
+    ),
+    CatModel(
+      title: 'Fat Cat',
+      imageURL: 'assets/images/cat3.jpg',
+    ),
+    CatModel(
+      title: 'Tai Tai',
+      imageURL: 'assets/images/cat4.jpg',
+    ),
+    CatModel(
+      title: 'Ter Ter',
+      imageURL: 'assets/images/cat5.jpg',
+    ),
+    CatModel(
+      title: 'Ly Ly',
+      imageURL: 'assets/images/cat6.jpg',
+    ),
+  ];
+}
