5729ba5111de95e203827b3ccd1ccf25655a7a8d
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-29 13:29:36 +0700

    Create showcupertio_controller.dart

---
+++ product_app/lib/showcupertino/controller/showcupertio_controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+
+class ShowController extends GetxController {
+  List<String> listLocat = [
+    'ភ្នំពេញ',
+    'តាកែវ',
+    'កំពង់ចាម',
+    'សៀមរាប',
+    'កណ្តាល',
+    'ស្វាយរៀង',
+    'កំពង់ស្ពឺ',
+    'ព្រះសីហុនុ',
+    'កំពត',
+    'កោះកុង',
+  ];
+  String? seletedListlocat = 'ភ្នំពេញ';
+  List<String> listType = [
+    'ឡូតិ៍កែង',
+    'ឡូតិ៍កណ្តាល',
+    'ឡូតិ៍ខាងក្រោយ',
+    'ឡូតិ៍ខាងមុខ',
+  ];
+  String? seletedType = 'ឡូតិ៍កែង';
+}
