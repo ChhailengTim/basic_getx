e6d5b017bc4ed07942fab2863d41422fe20aa765
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-08-29 15:15:28 +0700

    Update sale_detail.dart

--- product_app/lib/sale_detail/view/sale_detail.dart
+++ product_app/lib/sale_detail/view/sale_detail.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:product_app/sale_detail/controller/button_next.dart';
 
 class SaleDetailScreen extends StatelessWidget {
@@ -33,14 +35,12 @@ class SaleDetailScreen extends StatelessWidget {
                     'ឈ្មោះឡូរ\nកាលបរិច្ឆេទទិញ\nទីតាំង\nស្ថានភាព\nតម្លៃ\nឈ្មោះភ្នាក់ងារ\nឈ្មោះអតិថិជន\nភេទ\nលេខអត្តសញ្ញាណប័ណ្ណ\nលេខលិខិតឆ្លងដែន\nលេខទូរស័ព្ទ\nលេខទូរស័ព្ទជំនួស',
                     style: TextStyle(
                       fontSize: 18,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
                   Text(
                     ':A01\n:22 Dec 2022 . 23:40\n:ផ្លូវជាតិលេខ​៨ 8, ភូមិជ្រៃ\n:Approved\n\$50.000\n:សុក​ បូរ៉ា\n:រស្មី\n:ប្រុស\n:0000123456789\n:0000123456789\n:0123456789\n:0234567890',
                     style: TextStyle(
                       fontSize: 18,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
@@ -49,7 +49,70 @@ class SaleDetailScreen extends StatelessWidget {
                 height: 10,
               ),
               ButtonNext(
-                onTap: () {},
+                onTap: () {
+                  showCupertinoModalPopup(
+                    context: context,
+                    builder: (context) => ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                      ),
+                      child: Material(
+                        child: SizedBox(
+                          height: Get.height * 0.5,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Row(
+                                  children: [
+                                    const Text(
+                                      'ការទូទាត់ប្រាក់់',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    const Spacer(),
+                                    GestureDetector(
+                                      onTap: () {
+                                        Get.back();
+                                      },
+                                      child:
+                                          Image.asset('assets/project12/X.png'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Row(
+                                  children: const [
+                                    Text(
+                                      'ឈ្មោះអ្នកផ្ញើរ​ប្រាក់\nឈ្មោះអ្នកទទួលប្រាក់\nកាលបរិច្ឆេទទទួលប្រាក់\nចំនួនទឹកប្រាក់',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 30,
+                                    ),
+                                    Text(
+                                      ':រស្មី\n:បូរ៉ា\n:22 Dec 2022 . 23:40\n:\$50.000',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
                 text: 'បង្ហាញការទូទាត់',
               ),
               const SizedBox(
