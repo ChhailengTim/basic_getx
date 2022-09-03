27dabc5c2e3a23921fd86ffc5557144ea9b64d32
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-09-03 10:34:41 +0700

    Create screen_render.dart

---
+++ product_app/lib/switch_map/view/screen/screen_render.dart
@@ -0,0 +1,3 @@
+export 'package:product_app/switch_map/view/screen/first.dart';
+export 'package:product_app/switch_map/view/screen/second.dart';
+export 'package:product_app/switch_map/view/screen/third.dart';
