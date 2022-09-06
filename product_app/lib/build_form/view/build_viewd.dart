cbe09ea82737ecd71ab0c657fda3fe209c5c4c8b
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-09-06 13:19:05 +0700

    any

---
+++ product_app/lib/build_form/view/build_viewd.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BuildFormScreen extends StatelessWidget {
+  const BuildFormScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Build Form'),
+      ),
+    );
+  }
+}
