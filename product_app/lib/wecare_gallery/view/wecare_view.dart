bd883d0c0c7de6c85ee8dd0f9b390b0c6be19347
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-09-08 13:36:16 +0700

    wecar gallery view can't work

---
+++ product_app/lib/wecare_gallery/view/wecare_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WecareView extends StatelessWidget {
+  const WecareView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: const [
+          Text('data'),
+        ],
+      ),
+    );
+  }
+}
