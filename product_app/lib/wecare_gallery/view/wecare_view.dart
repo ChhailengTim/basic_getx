a6a65cb0d6b91ce8864a4e269f88b9acae43bbce
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-09-08 13:39:45 +0700

    Update wecare_view.dart

--- product_app/lib/wecare_gallery/view/wecare_view.dart
+++ product_app/lib/wecare_gallery/view/wecare_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:product_app/wecare_gallery/view/wecar_gallery.dart';
 
 class WecareView extends StatelessWidget {
   const WecareView({Key? key}) : super(key: key);
@@ -9,6 +10,14 @@ class WecareView extends StatelessWidget {
       appBar: AppBar(),
       body: Column(
         children: const [
+          ViewImageFullScreen(
+            images: [
+              'assets/images/cat1.jpg',
+              'assets/images/cat3.jpg',
+            ],
+            index: 2,
+            isNetwork: false,
+          ),
           Text('data'),
         ],
       ),
