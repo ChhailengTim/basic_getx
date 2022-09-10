cdca3c1023a15e44ba2851e748fc6f4551d7763c
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-09-10 10:44:37 +0700

    Create row_column.dart

---
+++ product_app/lib/row_column/view/row_column.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class RowColumnScreen extends StatelessWidget {
+  const RowColumnScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Row Column'),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black12,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.only(
+                            left: 50,
+                            right: 50,
+                            top: 10,
+                            bottom: 10,
+                          ),
+                          child: Icon(Icons.abc),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black12,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.only(
+                            left: 50,
+                            right: 50,
+                            top: 10,
+                            bottom: 10,
+                          ),
+                          child: Icon(Icons.abc),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.only(
+                        left: 50,
+                        right: 50,
+                        top: 100,
+                        bottom: 100,
+                      ),
+                      child: Icon(Icons.abc),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
