b7dc1fd2e669ff54e316e74c1091c297ca0a20af
CHHAILENG TIM <chhailengtim2018@gmail.com>
2022-10-01 09:14:45 +0700

    Update project_detail.dart

--- product_app/lib/api_dio/view/widgets/projects/project_detail.dart
+++ product_app/lib/api_dio/view/widgets/projects/project_detail.dart
@@ -22,10 +22,42 @@ class ProjectDetail extends StatelessWidget {
             width: Get.width,
             height: (Get.width - 16) / 2.5,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [],
-          ),
+          if (projectModel.imageGallery != null)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                for (int index = 0; index < 3; index++)
+                  projectModel.imageGallery!.length > index
+                      ? Stack(
+                          children: [
+                            index < 3
+                                ? imageCached(
+                                    'https://test-pms-chhaythai-api.idev.group/images/project/${projectModel.imageGallery?[index].name}',
+                                    width: 100,
+                                    height: 100,
+                                  )
+                                : const SizedBox(),
+                            projectModel.imageGallery!.length > 3 && index == 2
+                                ? Positioned(
+                                    child: Container(
+                                      width: 100,
+                                      height: 100,
+                                      color: Colors.white24,
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '+${projectModel.imageGallery!.length - 3}',
+                                        style: const TextStyle(
+                                          fontSize: 15,
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                : const SizedBox(),
+                          ],
+                        )
+                      : const SizedBox(),
+              ],
+            ),
         ],
       ),
     );
